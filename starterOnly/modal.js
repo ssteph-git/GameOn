ce8aa0c46b5fc0f678141cac300eb810cb1bd85f
SIARRAS Stéphane <siarras.stephane@gmail.com>
2022-04-25 20:22:48 -0400

    fermeture du formulaire: valide

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,7 +10,10 @@ function editNav() {
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
+const modalBtClose = document.querySelector(".close");//Sélection du bouton de fermeture du formulaire
 const formData = document.querySelectorAll(".formData");
+
+console.log(modalbg);
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +21,12 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+  modalBtClose.addEventListener('click',closeModal);//appel de la fonction closeModal, si on clic sur le bouton de fermeture
 }
 
+function closeModal() {
+
+  modalbg.style.display = "none"; //changement de la propriété display bloc en display none: pour supprimer le formulaire du Dom
+
+}
 
