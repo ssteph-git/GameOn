1e8dd7e1b7997b69617642d56e220233d90e8959
SIARRAS Stéphane <siarras.stephane@gmail.com>
2022-05-10 06:06:52 -0400

    responsive OK

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,25 +1,95 @@
+// var element = element.getElementsByClassName('icon');
+
+const icon = document.getElementById('icon');
+icon.addEventListener('click',editNav);
+// icon.addEventListener('click',editNav);
+
+const icon2 = document.getElementById('icon2');
+icon2.addEventListener('click',editNav2);
+
+const icon3 = document.getElementById('icon3');
+icon3.addEventListener('click',editNav3);
+
+// const icon2 = document.getElementsByClassName('icon');
+// console.log(icon2);
+// icon2[0].addEventListener("click", editNav('myTopnav'));
+// icon2[1].addEventListener("click", editNav('myTopnav2'));
+
+
+// const iconModal = document.getElementsByClassName('iconModal');
+// iconModal[0].addEventListener("click", editNav('myTopnav2'));
+
+// console.log(iconModal);
+
+
 function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
+  console.log('editNav!!');
+  console.log();
+  let x = document.getElementById('myTopnav');
+  if (x.className == "topnav") {
     x.className += " responsive";
+    console.log(x.className);
   } else {
     x.className = "topnav";
   }
 }
 
+function editNav2() {
+  console.log('editNav!!');
+  console.log();
+  let x = document.getElementById('myTopnav2');
+  if (x.className == "topnav") {
+    x.className += " responsive";
+    console.log(x.className);
+  } else {
+    x.className = "topnav";
+  }
+}
+
+function editNav3() {
+  console.log('editNav!!');
+  console.log();
+  let x = document.getElementById('myTopnav3');
+  if (x.className == "topnav") {
+    x.className += " responsive";
+    console.log(x.className);
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// function editNav(e) {
+//   console.log('editNav!!');
+//   console.log(e);
+//   let x = document.getElementById(e);
+//   if (x.className == "topnav") {
+//     x.className += " responsive";
+//     console.log(x.className);
+//   } else {
+//     x.className = "topnav";
+//   }
+// }
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const modalBtClose = document.querySelector(".close");//Sélection du bouton de fermeture du formulaire
+const modalBtCloseValide = document.querySelector(".valide");//Sélection du bouton de fermeture du formulaire
+
 const formData = document.querySelectorAll(".formData");
+
+const modalValide = document.querySelector(".bground-Valide");//Sélection du bouton de fermeture du formulaire
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtClose.addEventListener('click', closeModal);//appel de la fonction closeModal, si on clic sur le bouton de fermeture
+modalBtCloseValide.addEventListener('click', closeValideModal);
+
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
-  modalBtClose.addEventListener('click', closeModal);//appel de la fonction closeModal, si on clic sur le bouton de fermeture
+  
 }
 
 function closeModal() {
@@ -28,8 +98,15 @@ function closeModal() {
 
 }
 
-function validate() {
+function closeValideModal() {
 
+  modalValide.style.display = "none"; //changement de la propriété display bloc en display none: pour supprimer le formulaire du Dom
+  
+}
+
+function validate(e) {
+// e.preventDefault();
+// e.stopPropagation();
   let firstName = document.forms["reserve"]["first"]; //Récupération de l'entré tapé au clavier
   let lastName = document.forms["reserve"]["last"];
   let eMail = document.forms["reserve"]["email"];
@@ -41,7 +118,7 @@ function validate() {
   let nbFirstName = firstName.value; 
   let nbLasttName = lastName.value; 
 
-
+let notValidate = 6;
 
   if (nbFirstName.length < 2) {
 
@@ -52,6 +129,7 @@ function validate() {
   }
   else {
 
+    notValidate--;
     formData[0].setAttribute("data-error-visible", "false");
     valeur = true;
 
@@ -64,6 +142,7 @@ function validate() {
     valeur = false;
   }
   else {
+    notValidate--;
     formData[1].setAttribute("data-error-visible", "false");
     valeur = true;
 
@@ -86,6 +165,7 @@ function validate() {
       valeur = false;
     }
     else{
+      notValidate--;
     formData[2].setAttribute("data-error-visible", "false");
     valeur = true;
     }
@@ -113,7 +193,7 @@ function validate() {
     // formData[2].setAttribute("data-error-visible", "false");
     // valeur = true;
     // }
-
+    notValidate--;
     formData[3].setAttribute("data-error-visible", "false");
     valeur = true;
 
@@ -126,6 +206,7 @@ function validate() {
     valeur = false;
   }
   else {
+    notValidate--;
     formData[4].setAttribute("data-error-visible", "false");
     valeur = true;
 
@@ -140,12 +221,27 @@ function validate() {
 
   }
   else {
-
+    notValidate--;
     formData[6].setAttribute("data-error-visible", "false");
 
   }
 
-
+if(notValidate==0){
+  alert('Parfait');
+  // submit.addEventListener('close',submit());
+  closeModal();
+  modalValide.style.display = "block";
+  //modalBtClose.addEventListener('click', closeModal);//appel de la fonction closeModal, si on clic sur le bouton de fermeture
+}
   // return false;
   return valeur;
 }
+
+// const formulaire = document.getElementById('reserve');
+const submit = document.getElementById('submit');
+console.log(submit);
+submit.addEventListener('click',validate);
+
+
+
+// submit.style.display="none";
